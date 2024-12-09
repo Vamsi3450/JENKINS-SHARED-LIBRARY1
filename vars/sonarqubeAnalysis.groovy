f4a8d89f3dfb4d08d5e7e5842cd67bd9eae161fc
VAMSI <144320777+Vamsi3450@users.noreply.github.com>
2024-12-09 12:10:15 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,11 +1,11 @@
 def call() {
     withSonarQubeEnv('sonar-server') {
         sh '''
-            $SCANNER_HOME/bin/sonar-scanner \
-            -Dsonar.projectKey=YouTube \
-            -Dsonar.sources=. \
-            -Dsonar.host.url=http://13.250.9.138:9000 \
-            -Dsonar.login=squ_12826907c29f5f324dba94e7726dd0605dc30270
+            $SCANNER_HOME/binsonar-scanner \
+        -Dsonar.projectKey=youtube \
+        -Dsonar.sources=. \
+        -Dsonar.host.url=http://98.81.242.166:9000 \
+        -Dsonar.login=squ_dc78e4ab5495bbc6fbe315600fda0900769be1c8
         '''
     }
 }
